17baea38ff39f26743671fd47fd3015133bde8f9
Ankit rustagi <ankitrustagi12@gmail.com>
2019-10-17 18:57:21 +0530

    Update Sherlock_and_the_beast.cpp

--- Sherlock_and_the_beast.cpp
+++ Sherlock_and_the_beast.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
+    int x;
+    cin>>x;
+    while(x--){
         int n,a=0,b=0;
         cin>>n;
         if(n%3==0)
-            {
-            a=n;b=0;
+        {
+           a=n;
+           b=0;
         }
-        else{a=n;
+        else{
+           a=n;
         while(a%3!=0){
-            a-=5;
-            b+=5;
+            a -= 5;
+            b += 5;
         }
         }
         if(b<=n){
